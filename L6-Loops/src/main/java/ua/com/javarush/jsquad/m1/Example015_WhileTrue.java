e4f8af48fa0e70114a2371348f008effb5d82d4d
oleksandr-jr <oleksandr.cho@gmail.com>
2026-03-11 09:03:15 +0100

    L6 - Loops

---
+++ L6-Loops/src/main/java/ua/com/javarush/jsquad/m1/Example015_WhileTrue.java
@@ -0,0 +1,23 @@
+package ua.com.javarush.jsquad.m1;
+
+import java.util.Scanner;
+
+public class Example015_WhileTrue {
+
+  public static void main(String[] args) {
+
+    Scanner scanner = new Scanner(System.in);
+
+    int couter = 1;
+
+//    System.out.println(scanner.hasNext());
+
+    while ( true ){
+
+    }
+
+
+//    System.out.println("End!");
+
+  }
+}
